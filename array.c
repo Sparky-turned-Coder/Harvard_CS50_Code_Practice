a10b7c2293830b9ff82736e678c1918919e63567
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2025-10-02 15:54:26 -0500

    got cs50.h working on local machine. now we can continue practicing on problem sets outside of codespace

--- array.c
+++ array.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void print_row(int bricks);
+
 int main(void)
 {
     int height = get_int("Pick a number: ");
 
     for (int i = 0; i < height; i++)
+    {
+        print_row(i + 1);
+    }
+}
+
+void print_row(int bricks)
+{
+    for (int i = 0; i < bricks; i++)
     {
         printf("#");
     }
